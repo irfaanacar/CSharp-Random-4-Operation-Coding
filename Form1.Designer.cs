3fba24d1cfeba10b9b2729a33eb78e8f9e05d4bf
Irfan <irfaanacar@gmail.com>
2023-09-14 01:01:43 +0300

    CSharp Random 4 Operation Coding
    This time there are 3 different labels. The properties of the first two labels will be useful for numbers to be added. Another label is required for 4 different operations. Thanks to these 3 labels that produce random values, random operations are provided to the user, and they should not forget that they have a certain time while performing these operations. By The Way, Sorry for my English.

---
+++ Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Rastgele4islem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(97, 119);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Oswald", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(186, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 51);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "+";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(264, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 46);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "0";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(305, 125);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(148, 40);
+            this.textBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(624, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 46);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.RoyalBlue;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Oswald", 14F, System.Drawing.FontStyle.Bold);
+            this.button1.Location = new System.Drawing.Point(126, 173);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 61);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "BAŞLA!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.SeaGreen;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("Oswald", 14F, System.Drawing.FontStyle.Bold);
+            this.button2.Location = new System.Drawing.Point(470, 114);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 61);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "CEVAPLA";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(707, 23);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(81, 46);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "PUAN";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(730, 69);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 46);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Khaki;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Oswald", 12F, System.Drawing.FontStyle.Bold);
+            this.button3.Location = new System.Drawing.Point(612, 404);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(176, 70);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "TEKRAR OYNA";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label7.Location = new System.Drawing.Point(12, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(183, 46);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "KALAN ZAMAN:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Oswald", 16F, System.Drawing.FontStyle.Bold);
+            this.label8.Location = new System.Drawing.Point(210, 23);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(49, 46);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "30";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(812, 495);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rastgele4islem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rd=new Random();
+        int puan = 0;
+        int kalanzaman = 30;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Text = "SONRAKİ";
+            timer1.Start();
+            int sayi1, sayi2, islem,cevap;
+            textBox1.Enabled = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            textBox1.Enabled = true;
+            sayi1 = Convert.ToInt32(label1.Text); 
+            sayi2 = Convert.ToInt32(label3.Text);
+            cevap=Convert.ToInt32(label4.Text);
+            sayi1 = rd.Next(0, 51);
+            label1.Text = sayi1.ToString();
+            sayi2 = rd.Next(0, 51);
+            label3.Text = sayi2.ToString();
+            islem = rd.Next(1,5);
+            label2.Text = islem.ToString();
+            if(islem == 1)
+            {
+                label2.Text = "+";
+                cevap = sayi1 + sayi2;
+                label4.Text = cevap.ToString();
+            }
+            if(islem == 2)
+            {
+                label2.Text = "-";
+
+                cevap = sayi1 - sayi2;
+                label4.Text = cevap.ToString();
+
+            }
+            if (islem == 3)
+            {
+                label2.Text = "*";
+                cevap = sayi1 * sayi2;
+                label4.Text = cevap.ToString();
+
+            }
+            if (islem == 4)
+            {
+                label2.Text = "/";
+                cevap = sayi1 / sayi2;
+                label4.Text = cevap.ToString();
+
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            button1.Enabled = true;
+            textBox1.Enabled = false;
+            if (textBox1.Text == label4.Text)
+            {
+                puan = puan + 10;
+                label6.Text=puan.ToString();
+            }
+            else
+            {
+                puan -= 10;
+                label6.Text=puan.ToString();
+            }
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label8.Text = "30";
+            kalanzaman = 30;
+            button1.Enabled = true;
+            label1.Text = "0";
+            label2.Text = "";
+            label3.Text = "0";
+            puan = 0;
+            label6.Text = "0";
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label4.Visible = false;
+            button2.Enabled=false;
+            button3.Enabled= false;
+            textBox1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            kalanzaman--;
+            label8.Text = kalanzaman.ToString();
+            if (label8.Text == "0")
+            {
+                
+                timer1.Stop();
+                MessageBox.Show("Tebrikler." + label6.Text + " " + "puan alarak oyunu bitirdiniz");
+                button2.Enabled = false;
+                button3.Enabled = true;
+                textBox1.Enabled = false;
+            }
+        }
+    }
+}
